1a8745d39a0202c03bd34a5d383d0ae36dfd6d61
ondrejvelisek <ondrejvelisek@gmail.com>
2018-07-06 19:43:19 +0200

    CAN-6280 Remove backwards compatibility for barcharts

--- src/CleverCharts.js
+++ src/CleverCharts.js
@@ -2,14 +2,10 @@
 import Doughnut from "./doughnut/Doughnut";
 import Histogram from "./histogram/Histogram";
 import Line from "./line/Line";
-import BarLegacy from "./bar/Bar";
 import Barchart from "./barchart/components/Barchart";
-import BarchartBackport from "./barchart/components/BarchartBackport";
 
 export default {
-	Bar:BarchartBackport,
 	Barchart:Barchart,
-	BarLegacy:BarLegacy,
 
     Doughnut:Doughnut,
     Histogram:Histogram,
